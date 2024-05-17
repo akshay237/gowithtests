877dd2fbb75a10ecb75f183e5f8295fff72949b7
akshay237 <akshaybeniwal12@gmail.com>
2024-05-17 22:40:41 +0530

    added version for read files

--- gofiles/version1/file.go
+++ gofiles/version1/file.go
@@ -1,12 +1,76 @@
 package v1
 
-import "testing/fstest"
+import (
+	"errors"
+	"io"
+	"io/fs"
+	"testing/fstest"
+)
 
 type Post struct {
-	Title, Description, Body string
-	Tags                     []string
+	Title       string
+	Description string
+	Body        string
+	Tags        []string
 }
 
-func NewPostsFromFS(fileSystem fstest.MapFS) []Post {
-	return []Post{}
+func NewPostsFromFS(fileSystem fstest.MapFS) ([]Post, error) {
+	dir, err := fs.ReadDir(fileSystem, ".")
+	if err != nil {
+		return nil, err
+	}
+	var posts []Post
+	for _, f := range dir {
+		post, err := getPost(fileSystem, f.Name())
+		if err != nil {
+			return nil, err
+		}
+		posts = append(posts, post)
+	}
+	return posts, nil
+}
+
+// Refactor it two funcs getPost and newPost and pass more sensful arguments
+// func getPost(fileSystem fs.FS, f fs.DirEntry) (Post, error) {
+// 	postFile, err := fileSystem.Open(f.Name())
+// 	if err != nil {
+// 		return Post{}, err
+// 	}
+// 	defer postFile.Close()
+
+// 	postData, err := io.ReadAll(postFile)
+// 	if err != nil {
+// 		return Post{}, err
+// 	}
+// 	post := Post{Title: string(postData[7:])}
+// 	return post, nil
+// }
+
+// The function `getPost` opens a file from a file system and returns a `Post` object along with any
+// errors encountered.
+func getPost(fileSystem fs.FS, fileName string) (Post, error) {
+	postFile, err := fileSystem.Open(fileName)
+	if err != nil {
+		return Post{}, err
+	}
+	defer postFile.Close()
+	return newPost(postFile)
+}
+
+// The function `newPost` reads a post file, extracts the title from the content, and returns a `Post`
+// struct along with any errors encountered.
+func newPost(postFile io.Reader) (Post, error) {
+	postData, err := io.ReadAll(postFile)
+	if err != nil {
+		return Post{}, err
+	}
+	post := Post{Title: string(postData[7:])}
+	return post, nil
+}
+
+type StubFailingFS struct {
+}
+
+func (s StubFailingFS) Open(name string) (fs.File, error) {
+	return nil, errors.New("oh no i always fail")
 }
--- gofiles/version1/file_test.go
+++ gofiles/version1/file_test.go
@@ -3,14 +3,29 @@ package v1
 import (
 	"testing"
 	"testing/fstest"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestNewBlogPosts(t *testing.T) {
 	fs := fstest.MapFS{
-		"helloworld.md": {Data: []byte("hi")},
+		"helloworld.md": {Data: []byte("Title: Post 1")},
 	}
-	posts := NewPostsFromFS(fs)
+	posts, err := NewPostsFromFS(fs)
+	got := posts[0]
+	want := Post{Title: "Post 1"}
+	assertPost(t, got, want)
+	if err != nil {
+		t.Fatal(err)
+	}
 	if len(posts) != len(fs) {
 		t.Errorf("got %d posts, wanted %d posts", len(posts), len(fs))
+	}
+}
+
+func assertPost(t *testing.T, got, want Post) {
+	t.Helper()
+	if !cmp.Equal(got, want) {
+		t.Errorf("got:%v, wanted:%v", got, want)
 	}
 }
