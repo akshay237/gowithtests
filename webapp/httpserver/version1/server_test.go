96c1eb2827db3979b5ac9ce59cd9dc8778f433fd
aks <aks@victoria.com>
2024-06-25 12:06:12 +0530

    http server version1 added

---
+++ webapp/httpserver/version1/server_test.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+// type Handler interface {
+// 	ServeHTTP(ResponseWriter, *Request)
+// }
+
+type StubPlayerStore struct {
+	scores map[string]int
+}
+
+func (s *StubPlayerStore) GetPlayerScore(name string) int {
+	return s.scores[name]
+}
+
+func newGetScoreRequest(name string) *http.Request {
+	request, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/players/%s", name), nil)
+	return request
+}
+
+func assertResponseBody(t *testing.T, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %s but want %s", got, want)
+	}
+}
+
+func assertStatus(t testing.TB, got, want int) {
+	t.Helper()
+	if got != want {
+		t.Errorf("did not get correct status, got %d, want %d", got, want)
+	}
+}
+
+func TestGetPlayers(t *testing.T) {
+	store := StubPlayerStore{
+		map[string]int{
+			"ram":   20,
+			"flyod": 10,
+		},
+	}
+	server := &PlayerServer{&store}
+
+	testcases := []struct {
+		name               string
+		player             string
+		expectedHTTPStatus int
+		expectedScore      string
+	}{
+		{
+			name:               "ram's score",
+			player:             "ram",
+			expectedHTTPStatus: http.StatusOK,
+			expectedScore:      "20",
+		},
+		{
+			name:               "flyod's score",
+			player:             "flyod",
+			expectedHTTPStatus: http.StatusOK,
+			expectedScore:      "10",
+		},
+		{
+			name:               "shyam's score",
+			player:             "shyam",
+			expectedHTTPStatus: http.StatusNotFound,
+			expectedScore:      "0",
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			request := newGetScoreRequest(tc.player)
+			response := httptest.NewRecorder()
+			server.ServeHTTP(response, request)
+			assertStatus(t, response.Code, tc.expectedHTTPStatus)
+			assertResponseBody(t, response.Body.String(), tc.expectedScore)
+		})
+	}
+	t.Run("returns a particular player score", func(t *testing.T) {
+		request := newGetScoreRequest("ram")
+		response := httptest.NewRecorder()
+		server.ServeHTTP(response, request)
+		got := response.Body.String()
+		want := "20"
+		assertResponseBody(t, got, want)
+	})
+	t.Run("return's Flyod's score", func(t *testing.T) {
+		request := newGetScoreRequest("flyod")
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		got := response.Body.String()
+		want := "10"
+		assertResponseBody(t, got, want)
+	})
+}
